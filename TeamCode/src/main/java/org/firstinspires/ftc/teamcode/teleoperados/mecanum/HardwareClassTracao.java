364a097c2129fbd1e427d3414c065da4ac47dead
Enzo-Coutinho <enzo@stemos.com.br>
2023-07-19 16:33:39 -0300

    Atualizando classe mecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleoperados/mecanum/HardwareClassTracao.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleoperados/mecanum/HardwareClassTracao.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.teleoperados.mechanums;
+package org.firstinspires.ftc.teamcode.teleoperados.mecanum;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleoperados/mecanum/Mechanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleoperados/mecanum/Mechanum.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.teleoperados.mechanums;
+package org.firstinspires.ftc.teamcode.teleoperados.mecanum;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
